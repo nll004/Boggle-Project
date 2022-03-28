6e16ae1315afff3a5772f2e2519eb973e1bac2e3
nll004 <loninath17@gmail.com>
2022-03-28 15:49:27 -0400

    added some flask tests

--- tests/test.py
+++ tests/test.py
@@ -6,5 +6,25 @@ from boggle import Boggle
 
 class FlaskTests(TestCase):
 
-    # TODO -- write tests for every view function / feature!
+    def setUp(self):
+        '''Set up testing config before each test'''
+        app.config['TESTING'] = True
 
+    def test_game_home(self):
+        with app.test_client() as client:
+            with client.session_transaction() as change_session:
+                change_session['games_played'] = 10
+
+            resp = client.get('/')
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertEqual(session['games_played'], 11)
+
+    # def test_check_valid_word(self):
+    #     with app.test_client() as client:
+    #         resp = client.post("/ans-sub", data= {'Answer': 'wordaeithaet'})
+
+    #         html = resp.get_data(as_text=True)
+
+    #         self.assertEqual(resp.status_code, 200)
+            # self.assertIs(resp.data['Answer'], "not-word")
